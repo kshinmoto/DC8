706b43a4db6e2ea6374250d2db525bbc28698399
kshinmoto <60911325+kshinmoto@users.noreply.github.com>
2020-04-26 17:44:30 -0700

    Add files via upload

--- DC8.py
+++ DC8.py
@@ -1,18 +1,12 @@
-import numpy as np # linear algebra
+import nltk
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-# Plot / Graph stuffs
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.linear_model import LogisticRegression
-from sklearn import preprocessing
+from sklearn import linear_model
+from sklearn.ensemble import RandomForestClassifier
 
-
-# read in dataset
+# read in dataset, make sure to change path for file in your computer
 init_data = pd.read_csv('/Users/kaishinmoto/Downloads/winemag-data-130k-v2.csv')
 print("Length of dataframe before duplicates are removed:", len(init_data))
 
@@ -25,10 +19,37 @@ parsed_data.dropna(subset=['description','points','price'])
 print("Length of dataframe after NaNs are removed:", len(parsed_data))
 
 
-dp = parsed_data[['description','points','price']]
+dp = parsed_data[['description','points']]
+print(dp)
+
+# remove the punctuations from description and create new column called new_description
+dp = dp.assign(new_description = dp['description'].str.replace('[^\w\s]',''))
+
+# this section would be used to lemmatize the description, but I found that it lowered the accuracy
+    # you can uncomment and test it out for yourself if interested
+    # I also tried to take out stopwords but that lowered the accuracy as well
+'''w_tokenizer = nltk.tokenize.WhitespaceTokenizer()
+lemmatizer = nltk.stem.WordNetLemmatizer()
+
+def lemmatize_text(text):
+    return [lemmatizer.lemmatize(w,'v') for w in w_tokenizer.tokenize(text)]
+
+dp = dp.assign(lemma_description = dp['new_description'].apply(lemmatize_text))
+print(dp['lemma_description'])
+
+# Function to convert
+def listToString(s):
+    # initialize an empty string
+    str1 = " "
+    # return string
+    return (str1.join(s))
+# create final_description column of lemmatized words in a string form
+dp = dp.assign(final_description = dp['lemma_description'].apply(listToString))'''
+
+
 dp = dp.assign(description_length = dp['description'].apply(len))
 
-
+# same function used in example given
 def transform_points_simplified(points):
     if points < 84:
         return 1
@@ -46,23 +67,19 @@ dp = dp.assign(points_simplified = dp['points'].apply(transform_points_simplifie
 dp = dp.dropna()
 print(dp)
 
-
-X = dp['description']
+# assign x and y, if testing lemmatized version change new_description to final_description
+X = dp['new_description']
 y = dp['points_simplified']
 
 vectorizer = CountVectorizer()
 vectorizer.fit(X)
 
 X = vectorizer.transform(X)
-print('Shape of Sparse Matrix: ', X.shape)
-print('Amount of Non-Zero occurrences: ', X.nnz)
-# Percentage of non-zero values
-density = (100.0 * X.nnz / (X.shape[0] * X.shape[1]))
-print('Density: {}'.format((density)))
 
+# print final version of dp being trained and tested, if doing lemmatized version change new_description to final_description
+print("Final DP: \n", dp['new_description'])
 
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1,random_state=42)
+train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.1,random_state=10 )
 
 
 from sklearn.naive_bayes import MultinomialNB
@@ -70,16 +87,35 @@ from sklearn.naive_bayes import MultinomialNB
 mnb = MultinomialNB()
 
 #Train the model using the training sets
-mnb.fit(X_train, y_train)
+mnb.fit(train_x, train_y)
 
 #Predict the response for test dataset
-y_pred = mnb.predict(X_test)
+y_pred = mnb.predict(test_x)
 
 #Import scikit-learn metrics module for accuracy calculation
 from sklearn import metrics
 
 # Model Accuracy, how often is the classifier correct?
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("MN Naive Bayes Accuracy: ",metrics.accuracy_score(test_y, y_pred))
 
+# Train multi-classification model with logistic regression
+lr = linear_model.LogisticRegression(max_iter= 1000)
+lr.fit(train_x, train_y)
+
+# Train multinomial logistic regression model
+mul_lr = linear_model.LogisticRegression(multi_class='multinomial', solver='newton-cg').fit(train_x, train_y)
+
+#print("Logistic regression Train Accuracy: ", metrics.accuracy_score(train_y, lr.predict(train_x)))
+print("Logistic regression Test Accuracy: ", metrics.accuracy_score(test_y, lr.predict(test_x)))
+#print("Multinomial Logistic regression Train Accuracy: ", metrics.accuracy_score(train_y, mul_lr.predict(train_x)))
+print("Multinomial Logistic regression Test Accuracy: ", metrics.accuracy_score(test_y, mul_lr.predict(test_x)))
+
+# compare results to RFC
+rfc = RandomForestClassifier()
+rfc.fit(train_x, train_y)
+
+# Testing the model
+predictions = rfc.predict(test_x)
+print("Randon Forest Classifier: ", metrics.accuracy_score(test_y, predictions))
 
 
